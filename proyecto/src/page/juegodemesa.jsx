05a5564771ad0d448a742d87a6372e089272ddb0
Eram <maa.rojasp@duocuc.cl>
2025-10-16 01:19:29 -0300

    uola

--- proyecto/src/page/juegodemesa.jsx
+++ proyecto/src/page/juegodemesa.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../styles/categorias.css'
 import {Link} from 'react-router-dom' 
+import Juegomesa111 from '../images/juegomesa111.jpg';
+import Juegomesa222 from '../images/juegomesa222.jpg';
+import Juegomesa333 from '../images/juegomesa333.jpg';
 
 export default function juegodemesa() {
   return (
@@ -30,7 +33,45 @@ export default function juegodemesa() {
 
         </nav>
     </header>
+    <article>
+     <img src={Juegomesa111} alt="" />   
+     <h2>Warhammer 40K: Set introductorio</h2>
+                <p >64.990</p>
+                <p>Un set introductorio de Warhammer 40,000 
+                    es una caja pensada para que principiantes 
+                    puedan iniciarse en el juego de miniaturas, 
+                    ya que incluye miniaturas (normalmente Marines Espaciales vs. Tiránidos), 
+                    un tapete de juego, dados, medidor de distancia, 
+                    reglas básicas y a veces herramientas de montaje y pintura,
+                     todo lo necesario para comenzar a jugar. </p>
 
+
+    </article>
+    <article>
+     <img src={Juegomesa222} alt="" />   
+     <h2>Monopoly: The Lord of the Rings</h2>
+                <p >79.990</p>
+                <p>En el juego de mesa Monopoly: Edición El Señor de los Anillos, 
+                    los jugadores imaginan estar en la Tierra Media
+                     como un miembro de la Comunidad. 
+                     Compra propiedades para proteger las tierras de los malvados
+                      ejércitos de Mordor y gana dinero cobrando alquileres
+                       y controlando el Anillo de Poder. </p>
+
+
+    </article>
+    <article>
+     <img src={Juegomesa333} alt="" />   
+     <h2>Zathura: Adventure is waiting</h2>
+                <p >134.990</p>
+                <p>Juego de mesa mágico y un juego de mesa que lleva
+                     a sus jugadores a un viaje espacial al planeta
+                      "Zathura", activando peligros y criaturas del espacio exterior, 
+                      incluyendo al astronauta, un robot descontrolado 
+                      y alienígenas de aspecto de lagarto. </p>
+
+
+    </article>
     <footer>
     <p>© 2025 Level-UP Gamer
     <br />
